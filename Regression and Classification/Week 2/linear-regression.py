1bb92aac2841cebfceba834a1de52f9dac034cb5
Aleksa <zabumaphu@gmail.com>
2026-05-18 15:06:51 -0400

    More code

---
+++ Regression and Classification/Week 2/linear-regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import time
+
+a = np.zeros(4)
+print(f"np.zeros(4) :   a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.zeros((4,))
+print(f"np.zeros(4,) :  a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.random_sample(4)
+print(f"np.random.random_sample(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+
+a = np.arange(4.)
+print(f"np.arange(4.):     a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+a = np.random.rand(4)
+print(f"np.random.rand(4): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
