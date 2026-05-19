fda62ead876c642395266e8d691a626ff1c6fc36
Aleksa <zabumaphu@gmail.com>
2026-05-19 19:11:25 -0400

    begin classification

--- Regression and Classification/Week 3/classifciation.py
+++ Regression and Classification/Week 3/classifciation.py
@@ -1,5 +1,16 @@
 """
-Description: Basic Classifier in python.
+Description: Basic Classifier in python to classify tumors.
 Author: Aleska Zatezalo
 Date: May 19, 2026
 """
+
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+
+# Import data
+data = load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+
+print(X.shape)
+print(y.value_counts())
